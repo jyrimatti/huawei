fc8b23af260aba533b8815bbbf5e1718d7413f85
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-23 11:29:19 +0200

    deps

--- cmd/total_active_power.sh
+++ cmd/total_active_power.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash bc netcat
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash bc netcat xxd
 set -eu
 
 dash ./cmd/modbus.sh TOTAL_ACTIVE_POWER Get
