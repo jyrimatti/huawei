7df0afcfd91f3940614417b082682631066668e0
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-29 20:14:32 +0300

    hourly and quarterly yields

---
+++ cmd/current_hourly_yield.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash sqlite
+set -eu
+
+dbfile="${1:-./huawei.db}"
+
+echo "SELECT MAX(measurement)-MIN(measurement)
+      FROM total_yield
+      WHERE instant >= strftime('%s', strftime('%Y-%m-%dT%H:00:00Z', 'now'))"\
+ | sqlite3 "$dbfile"\
+ | { read -r val; printf '%.3f\n' "$val"; }
+
---
+++ cmd/previous_quarterly_yield.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash sqlite
+set -eu
+
+dbfile="${1:-./huawei.db}"
+
+echo "SELECT MAX(measurement)-MIN(measurement)
+      FROM total_yield
+      WHERE instant >= strftime('%s', strftime('%Y-%m-%dT%H:%M:00Z', Datetime('now', '-'||(strftime('%M', 'now') % 15 + 15)||' minutes')))"\
+ | sqlite3 "$dbfile"\
+ | { read -r val; printf '%.3f\n' "$val"; }
+
