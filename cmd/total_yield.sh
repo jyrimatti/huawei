0daa20154954f86e8a7f6e42c497b325aba32fae
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-02-11 22:42:13 +0200

    ...

--- cmd/total_yield.sh
+++ cmd/total_yield.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep BKT_SCOPE --keep BKT_CACHE_DIR
-#! nix-shell -i dash -I channel:nixos-24.11-small -p nix dash bc netcat xxd bkt
+#! nix-shell -i dash -I channel:nixos-24.11-small -p nix dash bc netcat xxd bkt flock
 set -eu
 
 dash ./huawei_get.sh TOTAL_YIELD
--- huawei_get.sh
+++ huawei_get.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep BKT_SCOPE --keep BKT_CACHE_DIR
-#! nix-shell -i dash -I channel:nixos-24.11-small -p nix gnused dash bc netcat xxd bkt
+#! nix-shell -i dash -I channel:nixos-24.11-small -p nix gnused dash bc netcat xxd bkt flock
 set -eu
 
 object="$1"
 
-bkt --discard-failures --ttl 60s --stale 50s -- dash ./huawei_get_fetch.sh "$object"
+lock="${BKT_CACHE_DIR:-/tmp}/huawei.lock"
+
+flock "$lock" \
+    bkt --discard-failures --ttl 60s --stale 50s -- \
+        dash ./huawei_get_fetch.sh "$object"
